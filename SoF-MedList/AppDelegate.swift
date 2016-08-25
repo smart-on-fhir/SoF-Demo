def438719cc6add04167501ef763d020db5726fa
Travis Cummings <travis@istalusconsulting.com>
2016-08-25 14:41:13 -0600

    Added Settings section to select the repository to connect too

--- SoF-MedList/AppDelegate.swift
+++ SoF-MedList/AppDelegate.swift
@@ -14,18 +14,41 @@ import SMART
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
 	var window: UIWindow?
-	
-	lazy var smart: Client = Client(
-		baseURL: "https://fhir-api-dstu2.smarthealthit.org",
-		settings: [
-//			"client_id": "my_mobile_app",
-			"client_name": "SMART on FHIR iOS Medication Sample App",
-			"redirect": "smartapp://callback",
-			"logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
-//			"keychain": false,
-			"verbose": true,
-		]
-	)
+    
+    let hspcSandbox = Client(
+        baseURL: "https://api.hspconsortium.org/hspc/data",
+        settings: [
+            "client_id": "3cb9c849-3c07-4d52-869b-a38f4ce86402",
+            "redirect": "smartapp://callback",
+            "logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
+            "keychain": false,
+            "verbose": true,
+        ]
+    )
+    
+    let smartSandbox = Client(
+        baseURL: "https://fhir-api-dstu2.smarthealthit.org",
+        settings: [
+            //			"client_id": "my_mobile_app",
+            "client_name": "SMART on FHIR iOS Medication Sample App",
+            "redirect": "smartapp://callback",
+            "logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
+            //			"keychain": false,
+            "verbose": true,
+        ]
+    )
+
+    lazy var smart: Client = Client(
+        baseURL: "https://fhir-api-dstu2.smarthealthit.org",
+        settings: [
+            //			"client_id": "my_mobile_app",
+            "client_name": "SMART on FHIR iOS Medication Sample App",
+            "redirect": "smartapp://callback",
+            "logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
+            //			"keychain": false,
+            "verbose": true,
+        ]
+    )
 	
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
 		if NSString(string: UIDevice.currentDevice().systemVersion).doubleValue >= 8.0 {			// there is `UISplitViewController` on iOS 7 but not for iPhone
