7263b0e2f84fe4f76b3dc8eeaeba8936e796d2ec
Dave Carlson <dcarlson@xmlmodeling.com>
2017-02-21 14:46:22 -0700

    Update endpoint list

--- Endpoints.swift
+++ Endpoints.swift
@@ -12,40 +12,55 @@ import SMART
 
 func configuredEndpoints() -> [Endpoint] {
 	var endpoints = [Endpoint]()
-    
-    let careplan1 = Client(
-        baseURL: URL(string: "https://api3.hspconsortium.org/careplan1/open")!,
-        settings: [
-            "client_name": "SMART on FHIR iOS Sample App",
-            "redirect": "smartapp://callback",
-            "logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
-            ])
-    careplan1.authProperties.granularity = .patientSelectNative
-    careplan1.authProperties.embedded = true
-    endpoints.append(Endpoint(client: careplan1, name: "HSPC Care Plan"))
-    
-    let hspc = Client(
-        baseURL: URL(string: "https://api3.hspconsortium.org/fhirconnect14/open")!,
-        settings: [
-            "client_name": "SMART on FHIR iOS Sample App",
-            "redirect": "smartapp://callback",
-            "logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
-            ])
-    hspc.authProperties.granularity = .patientSelectNative
-    hspc.authProperties.embedded = true
-    endpoints.append(Endpoint(client: hspc, name: "HSPC"))
-    
-    let hapi = Client(
-        baseURL: URL(string: "https://fhirtest.uhn.ca/baseDstu3")!,
-        settings: [
-            "client_name": "SMART on FHIR iOS Sample App",
-            "redirect": "smartapp://callback",
-            "logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
-            ])
-    hapi.authProperties.granularity = .patientSelectNative
-    hapi.authProperties.embedded = true
-    endpoints.append(Endpoint(client: hapi, name: "HAPI Public"))
 	
+	let hspc = Client(
+		baseURL: URL(string: "https://api3.hspconsortium.org/fhirconnect14/open")!,
+		settings: [
+			"client_name": "SMART on FHIR iOS Sample App",
+			"redirect": "smartapp://callback",
+			"logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
+			])
+	hspc.authProperties.granularity = .patientSelectNative
+	hspc.authProperties.embedded = true
+	endpoints.append(Endpoint(client: hspc, name: "HSPC"))
+	
+	let hapi = Client(
+		baseURL: URL(string: "https://fhirtest.uhn.ca/baseDstu3")!,
+		settings: [
+			"client_name": "SMART on FHIR iOS Sample App",
+			"redirect": "smartapp://callback",
+			"logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
+			])
+	hapi.authProperties.granularity = .patientSelectNative
+	hapi.authProperties.embedded = true
+	endpoints.append(Endpoint(client: hapi, name: "HAPI Public"))
+	
+	// Grahame's test server
+	let grahame = Client(
+		baseURL: URL(string: "http://fhir3.healthintersections.com.au/open")!,
+		settings: [
+			"client_name": "SMART on FHIR iOS Sample App",
+			"redirect": "smartapp://callback",
+			"logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
+			])
+	grahame.authProperties.granularity = .patientSelectNative
+	grahame.authProperties.embedded = true
+	endpoints.append(Endpoint(client: grahame, name: "Health Intersections"))
+	
+	// SMART DSTU-2!! sandbox
+	// Credentials obtained by registering on the SMART website
+	let smart = Client(
+		baseURL: URL(string: "https://fhir-api-dstu2.smarthealthit.org")!,
+		settings: [
+			"client_id": "my_mobile_app",
+			"client_name": "SMART on FHIR iOS Sample App",
+			"redirect": "smartapp://callback",
+			"logo_uri": "https://avatars1.githubusercontent.com/u/7401080",
+			])
+	smart.authProperties.granularity = .patientSelectNative
+	smart.authProperties.embedded = true
+	endpoints.append(Endpoint(client: smart, name: "SMART"))
+
 	return endpoints
 }
 
